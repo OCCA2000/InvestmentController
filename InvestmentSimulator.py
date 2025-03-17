09fb1e0e14a671a635edf35bac213c81220b1c37
Cristian Orellana <orellana.cristian@hotmail.es>
2025-03-16 19:39:30 -0500

    Updating simulation table to sell the requeted investment.

--- InvestmentSimulator.py
+++ InvestmentSimulator.py
@@ -1,7 +1,9 @@
 import pymysql
+from datetime import datetime
 
 base_datos='investment'
 tabla='amortization'
+hoy=datetime.today()
 
 conn = pymysql.connect(host="localhost",user="root",passwd="",db=base_datos)
 
@@ -13,7 +15,20 @@ cursor.execute("CREATE TABLE amortization_sim AS SELECT * FROM amortization")
 
 inv_id = input("Ingrese el identificador de la inversión que desea vender: ")
 
-print(inv_id)
+sql="SELECT * FROM amortization_sim WHERE inv_id = %s AND am_expiration_date > %s"
+valores=(inv_id, hoy)
+
+cursor.execute(sql, valores)
+resultados = cursor.fetchall()
+
+for fila in resultados:
+    am_id=fila[0]
+    sql="UPDATE amortization_sim SET am_sold_date = %s, am_expired = 0, is_active = 0, is_deleted = 1 WHERE id = %s"
+    valores=(hoy, am_id)
+    cursor.execute(sql, valores)
+    conn.commit()
+
+print(f"Inversión {inv_id} vendida exitosamente.")
 
 conn.close()
 
