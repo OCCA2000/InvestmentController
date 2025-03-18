4fe468d0307af3f8a292f60bbcee931f1fce8b3a
Cristian Orellana <orellana.cristian@hotmail.es>
2025-03-17 22:37:17 -0500

    Improving the method for selling the investments.

--- InvestmentSimulator.py
+++ InvestmentSimulator.py
@@ -10,23 +10,15 @@ conn = pymysql.connect(host="localhost",user="root",passwd="",db=base_datos)
 cursor = conn.cursor()
 
 cursor.execute("DROP TABLE IF EXISTS amortization_sim")
-
 cursor.execute("CREATE TABLE amortization_sim AS SELECT * FROM amortization")
 
 inv_id = input("Ingrese el identificador de la inversión que desea vender: ")
 
-sql="SELECT * FROM amortization_sim WHERE inv_id = %s AND am_expiration_date > %s"
-valores=(inv_id, hoy)
+sql="UPDATE amortization_sim SET am_sold_date = %s, am_expired = 0, is_active = 0, is_deleted = 1 WHERE inv_id = %s AND am_expiration_date >= %s"
+valores=(hoy, inv_id, hoy)
 
 cursor.execute(sql, valores)
-resultados = cursor.fetchall()
-
-for fila in resultados:
-    am_id=fila[0]
-    sql="UPDATE amortization_sim SET am_sold_date = %s, am_expired = 0, is_active = 0, is_deleted = 1 WHERE id = %s"
-    valores=(hoy, am_id)
-    cursor.execute(sql, valores)
-    conn.commit()
+conn.commit()
 
 print(f"Inversión {inv_id} vendida exitosamente.")
 
