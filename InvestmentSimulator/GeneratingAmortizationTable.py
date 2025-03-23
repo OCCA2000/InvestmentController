ec9c7e5efcb8647cfdb050e1ab8f5f29dacf6ff5
Cristian Orellana <orellana.cristian@hotmail.es>
2025-03-23 16:29:44 -0500

    Creating the amortization table using the information stored in a database.

--- InvestmentSimulator/GeneratingAmortizationTable.py
+++ InvestmentSimulator/GeneratingAmortizationTable.py
@@ -2,24 +2,36 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import pandas as pd
 from datetime import datetime, timedelta
+import pymysql
+
+def connect_to_database():
+    database='inversiones'
+    conn = pymysql.connect(host="localhost",user="root",passwd="",db=database)
+    return conn
 
 def generate_files():
+    conn = connect_to_database()
+    cursor = conn.cursor()
     try:
         investment_id = entry_investment_id.get()
-        owner = entry_owner.get()
-        yield_value = float(entry_yield_value.get())
-        principal = float(entry_principal.get())
-        purchase_date = datetime.strptime(entry_purchase_date.get(), '%Y-%m-%d')
-        maturity_date = datetime.strptime(entry_maturity_date.get(), '%Y-%m-%d')
-        annual_interest_rate = float(entry_annual_interest_rate.get())
+        first_interest_payment_date = datetime.strptime(entry_first_interest_payment_date.get(), '%Y-%m-%d')
+        payment_frequency = int(entry_payment_frequency.get())
         try:
             capital_repayments_dates = [datetime.strptime(d.strip(), '%Y-%m-%d') for d in entry_capital_repayments_dates.get().split(',')]
         except:
             capital_repayments_dates = []
-        amount_paid = float(entry_amount_paid.get())
-        first_interest_payment_date = datetime.strptime(entry_first_interest_payment_date.get(), '%Y-%m-%d')
-        payment_frequency = int(entry_payment_frequency.get())
-        
+
+        sql="SELECT * FROM inversion WHERE id = %s"
+        valores=(investment_id)
+        cursor.execute(sql, valores)
+        values = cursor.fetchone()
+        columns = [i[0] for i in cursor.description]
+        investment = pd.DataFrame([values], columns=columns)
+        principal = investment['inv_monto_a_negociar'][0]
+        maturity_date = investment['inv_fecha_vencimiento'][0]
+        annual_interest_rate = investment['inv_tasa_interes'][0]
+        amount_paid = investment['inv_capital_invertido'][0]-investment['inv_valor_interes'][0]
+
         payment_dates = []
         interest_payments = []
         principal_remaining = []
@@ -29,7 +41,8 @@ def generate_files():
         monthly_interest_rate = annual_interest_rate / 100 / 12
         
         current_date = first_interest_payment_date
-        while current_date <= maturity_date:
+
+        while current_date.date() <= maturity_date:
             payment_dates.append(current_date)
             next_month = current_date.month + payment_frequency if current_date.month + payment_frequency <= 12 else current_date.month + payment_frequency - 12
             next_year = current_date.year if current_date.month + payment_frequency <= 12 else current_date.year + 1
@@ -44,13 +57,13 @@ def generate_files():
         remaining_principal = principal
         repayment_amount = principal / num_capital_repayments
         for date in payment_dates:
-            interest_payment = remaining_principal * monthly_interest_rate
+            interest_payment = remaining_principal * monthly_interest_rate * payment_frequency
             interest_payments.append(round(interest_payment, 2))
             if date in capital_repayments_dates:
                 remaining_principal -= repayment_amount
                 principal_returned.append(repayment_amount)
                 actual_principal_returned.append(actual_principal_return)
-                prize.append(repayment_amount-actual_principal_return)
+                prize.append(round(repayment_amount-actual_principal_return,2))
             else:
                 principal_returned.append(0)
                 actual_principal_returned.append(0)
@@ -68,9 +81,6 @@ def generate_files():
         })
         
         amortization_table["ID"] = investment_id
-        amortization_table["Propietario"] = owner
-        amortization_table["Rendimiento"] = yield_value
-        amortization_table["Fecha de Compra"] = purchase_date.strftime('%Y-%m-%d')
         amortization_table["Fecha de Vencimiento"] = maturity_date.strftime('%Y-%m-%d')
         amortization_table["Tasa nominal de interés anual"] = annual_interest_rate
         amortization_table["Amortización"] = amortization_table["Interés Mensual"] + amortization_table["Capital Devuelto"] + amortization_table["Premio"]
@@ -92,7 +102,8 @@ def generate_files():
         if file_path_sql:
             sql_statements = []
             for _, row in final_table.iterrows():
-                sql_statements.append(f"INSERT INTO amortization (inv_id, am_purchase_date, am_expiration_date, am_owner, am_enterprise, am_months, am_days, am_rate, am_return, am_interest, am_principal, am_retention, am_interest_total, am_sold_date, am_expired, is_active, is_deleted) VALUES ({row['ID']}, '{row['Fecha de Compra']}', '{row['Fecha de Pago']}', '{row['Propietario']}', 'BONOS DEL ESTADO {row['Fecha de Vencimiento']}', 0, 0, {round(row['Tasa nominal de interés anual'], 2)}, {round(row['Rendimiento'], 2)}, {round(row['Interés Mensual'], 2)}, {round(row['Capital Devuelto'], 2)}, 0, {round(row['Interés Mensual'], 2)}, NULL, 0, 0, 0);\n")
+                pass
+                #sql_statements.append(f"INSERT INTO amortization (inv_id, am_purchase_date, am_expiration_date, am_owner, am_enterprise, am_months, am_days, am_rate, am_return, am_interest, am_principal, am_retention, am_interest_total, am_sold_date, am_expired, is_active, is_deleted) VALUES ({row['ID']}, '{row['Fecha de Compra']}', '{row['Fecha de Pago']}', '{row['Propietario']}', 'BONOS DEL ESTADO {row['Fecha de Vencimiento']}', 0, 0, {round(row['Tasa nominal de interés anual'], 2)}, {round(row['Rendimiento'], 2)}, {round(row['Interés Mensual'], 2)}, {round(row['Capital Devuelto'], 2)}, 0, {round(row['Interés Mensual'], 2)}, NULL, 0, 0, 0);\n")
             
             with open(file_path_sql, "w") as sql_file:
                 sql_file.writelines(sql_statements)
@@ -103,7 +114,11 @@ def generate_files():
 root = tk.Tk()
 root.title("Generador de Tabla de Amortización")
 
-labels = ["Investment ID:", "Owner:", "Yield Value:", "Principal:", "Purchase Date (YYYY-MM-DD):", "Maturity Date (YYYY-MM-DD):", "Annual Interest Rate:", "Capital Repayment Dates (comma-separated YYYY-MM-DD):", "Amount Paid:", "First Interest Payment Date (YYYY-MM-DD):", "Payment frequency:"]
+labels = ["ID:",
+          "Primera fecha de pago (YYYY-MM-DD):", 
+          "Frecuencia de pago:",
+          "Fechas de retorno de capital (separadas por comas YYYY-MM-DD):",
+         ]
 entries = []
 
 for i, label in enumerate(labels):
@@ -112,7 +127,7 @@ for i, label in enumerate(labels):
     entry.grid(row=i, column=1, padx=10, pady=5)
     entries.append(entry)
 
-entry_investment_id, entry_owner, entry_yield_value, entry_principal, entry_purchase_date, entry_maturity_date, entry_annual_interest_rate, entry_capital_repayments_dates, entry_amount_paid, entry_first_interest_payment_date, entry_payment_frequency = entries
+entry_investment_id, entry_first_interest_payment_date, entry_payment_frequency, entry_capital_repayments_dates = entries
 
 tk.Button(root, text="Generar Amortización y SQL", command=generate_files).grid(row=len(labels), columnspan=2, pady=10)
 
